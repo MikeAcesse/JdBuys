8c88a466a6b7506d4baf6181bc8bb407d1821f38
fanzk <fanzk@info2soft.com>
2020-11-04 19:00:58 +0800

    readparams

---
+++ src/com/hp/servlet/ReadParams.java
@@ -0,0 +1,50 @@
+package com.hp.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+/**
+ * @author fanzk
+ * @version 1.8
+ * @date 2020/11/4 16:35
+ */
+@WebServlet("/ReadParams")
+public class ReadParams extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request,response);
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       //设置响应内容类型
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		String title="读取所有表单参数";
+		String docType =
+				"<!doctype html public \"-//w3c//dtd html 4.0 " +
+						"transitional//en\">\n";
+		out.println(docType +
+				"<html>\n" +
+				"<head><meta charset=\"utf-8\"><title>" + title + "</title></head>\n" +
+				"<body bgcolor=\"#f0f0f0\">\n" +
+				"<h1 align=\"center\">" + title + "</h1>\n" +
+				"<table width=\"100%\" border=\"1\" align=\"center\">\n" +
+				"<tr bgcolor=\"#949494\">\n" +
+				"<th>参数名称</th><th>参数值</th>\n"+
+				"</tr>\n");
+		Enumeration paraNames = request.getParameterNames();
+		while(paraNames.hasMoreElements()){
+			String paraName = (String) paraNames.nextElement();
+
+
+		}
+	}
+}
